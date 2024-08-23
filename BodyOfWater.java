89cb79f9923b20bf9ccf7646222011a94f5316bf
Shin96 <clarks25@ausdk12.org>
2024-08-23 17:03:20 +0000

    BodyOfWater HW

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,50 @@
+public class BodyOfWater {
+    private String name;
+    private double largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater(String n, double ldm, double avd, boolean isw){
+        name = n;
+        largestDiameter = ldm;
+        avgDepth = avd;
+        isSaltWater = isw;
+    }
+    
+    public BodyOfWater(String n, double avd){
+        name = n;
+        avgDepth = avd;
+    }
+    
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setlargestDiameter(double ldm){
+        largestDiameter = ldm;
+    }
+
+    public void setavgDepth(double avd){
+        avgDepth = avd;
+    }
+
+    public void setisSaltWater(boolean isw){
+        isSaltWater = isw;
+    }
+
+    public String name(){
+        return name;
+    }
+    
+    public double largestDiameter(){
+        return largestDiameter;
+    }
+
+    public double avgDepth(){
+        return avgDepth;
+    }
+
+    public boolean isSaltWater(){
+        return isSaltWater;
+    }
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,23 @@
 public class BodyOfWaterMain {
+    //public static void main(String[] args) {
+        //BodyOfWater b = new BodyOfWater();
+        //System.out.println("here");
+   // }
+
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Atlantic", 666, 16.6, false);
+        BodyOfWater b1 = new BodyOfWater("Pacific", 555, 15.5, false );
+
+        System.out.println(b.name());
+        System.out.println(b1.name());
+
+        System.out.println(b.largestDiameter());
+        System.out.println(b1.largestDiameter());
+
+        System.out.println(b.avgDepth());
+        System.out.println(b1.avgDepth());
+
+        System.out.println(b.isSaltWater());
+        System.out.println(b1.isSaltWater());
     }
 }
